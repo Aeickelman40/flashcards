1a290b1d5e4c2731874ba22a2154c64c15f80bcc
Alex Eickelman <aeickelman40@gmail.com>
2020-05-06 19:05:24 -0600

    Finish game class, still needs overall refactor

--- src/Game.js
+++ src/Game.js
@@ -1,12 +1,28 @@
 const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
+const Card = require('../src/Card');
+const Turn = require('../src/Turn');
+const Deck = require('../src/Deck');
+const Round = require('../src/Round');
 
 class Game {
-  constructor() {}
+  constructor() {
+    this.roundAmount = 0;
+    this.currentRound;
+  }
 
-  printMessage(deck, round) {
-      console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
+  start() {
+    this.roundAmount++;
+    const cards = prototypeQuestions.map(card => new Card(card.id, card.question, card.answers, card.correctAnswer));
+    const deck = new Deck(cards);
+    this.currentRound = new Round(deck);
+    this.printMessage(deck, this.currentRound);
+    this.printQuestion(this.currentRound);
+  }
+
+  printMessage(deck) {
+      console.log(`Welcome to FlashCards! You are playing with ${deck.countDeck()} cards. You are currently on Round #${this.roundAmount}!
 -----------------------------------------------------------------------`)
   }
 
---
+++ test/Game-test.js
@@ -0,0 +1,31 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Card = require('../src/Card');
+const Turn = require('../src/Turn');
+const Deck = require('../src/Deck');
+const Round = require('../src/Round');
+const Game = require('../src/Game');
+const protoTypeQuestions = require('../src/data')
+
+describe('Game', function() {
+  it('should be a function', function() {
+    const game = new Game() ;
+    expect(Game).to.be.a('function')
+  })
+
+  it('should be an instance of a Game', function() {
+    const game = new Game() ;
+    expect(game).to.be.an.instanceof(Game);
+  })
+
+  it('should be able to start a game', function() {
+    const cards = protoTypeQuestions.prototypeData
+    const deck = new Deck(cards);  
+    const round = new Round(deck);
+    const game = new Game();
+    game.start();
+    expect(deck.cards.length).to.equal(30);
+  })
+    
+})
